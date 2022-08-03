b39ac85e31bc0e195c1d285797388ce4a6d8025f
e.camalzade <e.camalzade@STHQ0118-04>
2022-08-03 13:07:58 +0400

    Add project files.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Parent to child 3 qayda
+            //Form melumat oturme qaydasi
+            // 1.Send data by constructor
+
+            //2.By property
+
+            //3. ShowDialog
+
+        }
+
+       Form2 form2 = new Form2();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Form2 form2 = new Form2(textBox1.Text);
+
+
+            form2.Text = textBox1.Text;
+            form2.Show();
+            //var result=form2.ShowDialog();
+
+            //if(result == DialogResult.OK)
+            //{
+            //    textBox1.Text = form2.DataForParent;
+            //}
+          // form2.Show();
+
+
+            //var user = new User
+            //{
+            //    Name = "Elvin",
+            //    Surname = "Camalzade",
+            //    Id = 1
+            //};
+
+
+            //Form2 form = new Form2();
+
+
+
+            //this.Hide();
+            //DialogResult a = form.ShowDialog(user);
+            //if (a == DialogResult.Cancel)
+            //{
+            //    this.Show();
+            //}
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = form2.DataForParent;
+        }
+    }
+}
---
+++ WindowsFormsApp2/Form2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        //public string Data { get; set; }
+        //public Form2(string text)
+        //{
+        //    InitializeComponent();
+        //    this.Data = text;  
+        //    label1.Text = Data;
+        //}
+
+
+        private string text;
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value;label1.Text = text; }
+        }
+
+
+        //public string Text { get; set;}
+        public Form2()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = Text;
+        }
+
+
+        public DialogResult ShowDialog(User user)
+        {
+            label1.Text = $"ID : {user.Id}  Name : {user.Name} Surname : {user.Surname}";
+            return ShowDialog();
+        }
+        public string DataForParent { get; set; }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //this.DialogResult= DialogResult.OK;
+            DataForParent = textBox1.Text;
+        }
+    }
+}
